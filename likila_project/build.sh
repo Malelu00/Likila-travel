dba4f5a750309d000b229c4b56f09c98fe551df3
Salang Malelu <77877522+Malelu00@users.noreply.github.com>
2026-07-11 20:55:15 +0200

    Delete likila_project/build.sh

--- likila_project/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Render runs this on every deploy
-set -o errexit
-
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py seed   # only adds missing data, safe to run on every deploy
