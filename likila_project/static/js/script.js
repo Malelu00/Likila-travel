19e76efd58612906ca4cedf1dc41e89f8f76ce58
Salang Malelu <77877522+Malelu00@users.noreply.github.com>
2026-07-11 20:29:22 +0200

    Update script.js

--- likila_project/static/js/script.js
+++ likila_project/static/js/script.js
@@ -1,5 +1,5 @@
-/*const API_BASE = '/api'; */
-const API_BASE = 'http://127.0.0.1:8000/api';
+const API_BASE = '/api'; 
+
 
 const navToggle = document.getElementById('nav-toggle');
 const navLinksEl = document.querySelector('.nav-links');
